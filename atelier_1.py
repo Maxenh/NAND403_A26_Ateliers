0a7cc352165fa6de5418b74a8bcb6e8d3f3a4bed
Maxenh <hamelink.tp@gmail.com>
2026-09-21 16:55:14 -0400

    Adding atelier_1

---
+++ atelier_1.py
@@ -0,0 +1,11 @@
+import sys
+
+#Demander le nom 
+print ("Quel est ton nom?")
+
+nom = input()
+
+print("Bienvenu a Cactusity", nom +'!')
+
+ville = input("d'où viens-tu?")
+print(ville+'?', "Quel nom étrange!")
